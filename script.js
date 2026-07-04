e09adf7154f64defa603a36b83b7ac7a7fbb9ce3
LokiThorAsgard <lokithorasgard@users.noreply.github.com>
2026-07-04 15:42:09 +0200

    Add hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 console.log("Loving Axolotl está funcionando 🫧");
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+navLinks.addEventListener("click", () => {
+  navLinks.classList.remove("active");
+});
